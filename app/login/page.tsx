af5425cbcc30e3af53ace704ca86eae7694e0de8
Lucas Ditchun <lucas.ditchun@gmail.com>
2024-11-11 16:18:21 -0300

    Login OTP criado no front-end

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,18 @@
-"use client"; // Adicione esta linha no topo do arquivo
+// app\login\page.tsx
+"use client";
 
 import { Button } from "../_components/ui/button";
 import { createClient } from "@/utils/supabase/client";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../_components/ui/card";
+import { Label } from "../_components/ui/label";
+import { Input } from "../_components/ui/input";
 
 const supabase = createClient();
 
@@ -16,9 +27,33 @@ async function signInWithGoogle() {
 
 function LoginPage() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <h1>Login</h1>
-      <Button onClick={signInWithGoogle}>Login com Google</Button>
+    <div className="grid min-h-screen items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+      <Card className="w-96">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>
+            Faça login ou cadastre-se usando apenas seu email ou conta Google.{" "}
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <Label>E-mail:</Label>
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="email@exemplo.com"
+              autoComplete="email"
+            />
+            <Button>Magic Login</Button>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant={"outline"} onClick={signInWithGoogle}>
+            Login com Google
+          </Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
